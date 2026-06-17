8e815a41a9b814a971dacc06198e1c624a7bfaa7
angeltubera <angeltubera315@gmail.com>
2026-06-17 14:44:02 +0800

    hi

---
+++ Basic_Structure_and_Syntax/activity1.java
@@ -0,0 +1,26 @@
+package Basic_Structure_and_Syntax;
+
+
+public class activity1 {
+    public static void main(String[] args) {
+        
+        System.out.println("im angel Tubera");
+        System.out.println("i am 18 years old");
+        System.out.println(108686120122l);
+        System.out.println("i am from Philippines");
+        System.out.println("i am a student");
+        System.out.println("IM A PROGRAMMER");
+        System.out.println("i like to play video games");
+        System.out.println("Addition");// This is a addition 
+        System.out.println(3 + 3);
+        System.out.println("Subtraction");// This is a subtraction 
+        System.out.println(10 - 5);
+        System.out.println("Multiplication");// This is a multiplication
+        System.out.println(4 * 6);
+        System.out.println("Division");// This is a division
+        System.out.println(15 / 3);
+
+
+    }
+
+}
