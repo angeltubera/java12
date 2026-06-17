0ddce5c3fd5d46d7f38f61c5377b823df043f5d0
angeltubera <angeltubera315@gmail.com>
2026-06-17 13:04:13 +0800

    Myfirstact

---
+++ Basic_Structure_and_Syntax/HelloWorld.java
@@ -0,0 +1,11 @@
+package Basic_Structure_and_Syntax;// This is a simple Java program that prints "Hello, World!" to the console.
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("im angel Tubera");
+        System.out.println("i am 18 years old");
+        System.out.println("i am from Philippines");
+        System.out.println("i am a student");
+        System.out.println("i am a programmer");
+    }
+}
