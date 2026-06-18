7344169f995d705260370d9ea16c349460b4ede1
angeltubera <angeltubera315@gmail.com>
2026-06-18 14:43:48 +0800

    Add HelloWorld, another, and variables classes; remove README.md

---
+++ Basic_Structure_and_Syntax/another.java
@@ -0,0 +1,13 @@
+package Basic_Structure_and_Syntax;
+
+public class another {}
+
+    public static void main(String[] args) {
+        System.out.println("This is another class in the Basic_Structure_and_Syntax package.");
+        String name = "baked potato";
+        System.out.println("Name: " + name);
+        System.out.println("This class is used to demonstrate that we can have multiple classes in the same package.");
+        
+        
+    }
+    
---
+++ Basic_Structure_and_Syntax/variables.java
@@ -0,0 +1,29 @@
+package Basic_Structure_and_Syntax;
+
+public class variables {
+    public static void main(String[] args) {
+      String name = "angel tubera";
+      int age = 18;//
+      final int score = 10;
+      String subject = "Mathematics"; // declaring variables with initialization
+      System.out.println("Name:" + name);
+      System.out.println("age:" + age);
+      System.out.println ("Score:" + score);
+      System.out.println("Subject:" + subject);
+      age = 18; //reassigning a new value to the variable age
+      // score = 20; // cannot reassign a final variable
+      System.out.println("New age:" + age);
+      Float height = 5.8f; // declaring a float variable
+      System.out.println("Height:" + height);
+      char section = 'A'; // declaring a char variable
+      System.out.println("Section:" + section);
+      boolean isStudent = true; // declaring a boolean variable
+      System.out.println("Is Student:" + isStudent);
+      
+
+
+      
+        
+    }
+}
+
